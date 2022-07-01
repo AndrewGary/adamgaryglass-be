f687b7300d69b43cf23ebf88955a6da5048309dd
Andrew Gary <Andrew.gary91@gmail.com>
2022-07-01 15:42:24 -0500

    product upload working correctly now, folder structure redone and much more organized now

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,12 +1,41 @@
 const asyncHandler = require('express-async-handler')
 
+const Product = require('../models/productsModel');
+const { cloudinary } = require('../utils/cloudinary');
+
 const getProducts = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Get Products'})
+    const products = await Product.find();
+
+    res.status(200).json(products)
 })
 
 const addProduct = asyncHandler(async (req, res) => {
-    console.log(req.body);
-    res.status(200).json(req.body)
+    let productBeingAdded = {
+        name: req.body.data.name,
+        description: req.body.data.description,
+        price: req.body.data.price,
+        category: req.body.data.category,
+        images: []
+    }
+    const imageURLs = []
+
+    for(let i = 0; i < req.body.data.images.length; i++){
+        const uploadedResponse = await cloudinary.uploader.upload(req.body.data.images[i], {
+            upload_preset: 'dev_setups'
+        })
+
+        imageURLs.push(uploadedResponse.secure_url);
+    }
+
+    productBeingAdded.images = imageURLs;
+
+    console.log('productBeingAdded: ', productBeingAdded);
+
+    const product = await Product.create(productBeingAdded);
+
+    console.log('product after upload: ', product);
+
+    res.status(200).json(product);
 })
 
 const updateProduct = asyncHandler(async (req, res) => {
--- index.js
+++ index.js
@@ -2,16 +2,24 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 9000;
 const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db');
+
+const cors = require('cors');
+
+connectDB();
 
 const productRouter = require('./routers/productRouter');
 
 const app = express();
+app.use(cors());
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb'}));
 app.use(express.urlencoded({ extended: false}))
 
 app.use('/api/products', productRouter);
 
+
+app
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
 })
